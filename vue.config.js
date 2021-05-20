498e7608272876187080b88680627687af0a4f39
Geocentric <geocentricllc@users.noreply.github.com>
2021-05-20 17:14:56 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
+  disableHostCheck: true,
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
